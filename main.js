53b8cab42e026e64cfadfe4f4e23edd2da6930c2
Elvis Farias <elvisrfarias1@gmail.com>
2023-04-16 17:43:34 -0300

    Removido codigos comentados)

--- main.js
+++ main.js
@@ -38,22 +38,3 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
 
 
-
-
-// // Enquanto
-// while (contador < listaDeTeclas.length) {
-//   const tecla = listaDeTeclas[contador];
-//   const instrumento = tecla.classList[1];
-//   const idAudio = `#som_${instrumento}`;
-
-//   tecla.onclick = function() {
-//     tocaSom(idAudio)
-//   }
-//   contador++
-//}
-
-
-
-
-
-
