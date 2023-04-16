f3a9ca998a10a1241c3bbf1aa5d640d7cb0f4631
Elvis Farias <elvisrfarias1@gmail.com>
2023-04-16 17:41:20 -0300

    Ajustes finais realizados e adicionado validações)

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-function tocaSom (idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+// Função para pegar o seletor e dar o play
+function tocaSom (seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if(elemento && elemento.localName === "audio"){
+    elemento.play()
+  }
+  else{
+    console.log("Elemento não encontrado! :/")
+  }
 }
 
+// Armazenando a lista
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-//para
+//Looping que percorre a lista de audio e botões
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-  const idAudio = `#som_${instrumento}`; //template string
+  const idAudio = `#som_${instrumento}`;
 
   tecla.onclick = function () {
       tocaSom(idAudio);
   }
 
+  tecla.onkeydown = function (event) {
+    if (event.code === 'Space' || event.code === 'Enter'){
+      tecla.classList.add('ativa')
+    }
+  }
+  tecla.onkeyup = function (event) {
+    if (event.code === 'Space' || event.code === 'Enter'){
+      tecla.classList.remove('ativa')
+    }
+  }
 }
 
 
